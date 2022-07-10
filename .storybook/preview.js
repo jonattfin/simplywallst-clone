4a7a9b5493d100dbaf2c6c0ab9557e909d7efc9d
jonattfin <jonattfin@outlook.com>
2022-07-10 20:14:43 +0300

    added storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +8,29 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: { ...getCustomViewports(), ...MINIMAL_VIEWPORTS }, // newViewports would be an ViewportMap. (see below for examples)
+    defaultViewport: "fhd",
+  },
+  backgrounds: {
+    default: "SimplyWallSt",
+    values: [
+      {
+        name: "SimplyWallSt",
+        value: "#151B24",
+      },
+    ],
+  },
+};
+
+function getCustomViewports() {
+  return {
+    fhd: {
+      name: "FHD",
+      styles: {
+        width: "1920px",
+        height: "1080px",
+      },
+    },
+  };
 }
